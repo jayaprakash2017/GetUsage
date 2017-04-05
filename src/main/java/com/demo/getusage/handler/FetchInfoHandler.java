749775a8c064a1a251a1b9657a10ece6c1a8d641
581727 <581727@01HW480339.India.TCS.com>
2017-04-05 10:19:50 +0530

    test commit

--- src/main/java/com/demo/getusage/handler/FetchInfoHandler.java
+++ src/main/java/com/demo/getusage/handler/FetchInfoHandler.java
@@ -23,7 +23,7 @@ public class FetchInfoHandler implements RequestHandler<GetInfoRequest, String>
         {
             GetInfoResponse getInfoResponse = new GetInfoResponse();
             getInfoResponse.setMsisdn(getInfoRequest.getMsisdn());
-            getInfoResponse.setName("Lamda Test User 68 accessed on 04-Apr");
+            getInfoResponse.setName("Lamda Test User 70 on 05-Apr");
             getInfoResponse.setVoiceUsage(new BigDecimal(100));
             getInfoResponse.setDataUsage(new BigDecimal("100.20"));
             ObjectMapper mapper = new ObjectMapper();
